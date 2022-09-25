e9b198bf60d5f74eb4a421faf37ae2d5774d7fed
lby132 <quddbscjswow@naver.com>
2022-09-25 23:51:12 +0900

    Kafka 적용(Consumer)

---
+++ src/main/java/com/example/catalogservice/messagequeue/KafkaConsumer.java
@@ -0,0 +1,47 @@
+package com.example.catalogservice.messagequeue;
+
+import com.example.catalogservice.jpa.CatalogEntity;
+import com.example.catalogservice.jpa.CatalogRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@Service
+@Slf4j
+public class KafkaConsumer {
+
+    CatalogRepository repository;
+
+    @Autowired
+    public KafkaConsumer(CatalogRepository repository) {
+        this.repository = repository;
+    }
+
+    @KafkaListener(topics = "example-catalog-topic") // example-catalog-topic 이름의 토픽으로 데이터가 전달되면 리스너가 잡아서 그 데이터를 가져온다.
+    public void updateQty(String kafkaMessage) {
+        log.info("Kafka Message: ->" + kafkaMessage);
+
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {});
+        } catch (JsonProcessingException ex) {
+            ex.printStackTrace();
+        }
+
+        CatalogEntity entity = repository.findByProductId((String) map.get("productId"));
+        if (entity != null) {
+            entity.setStock(entity.getStock() - (Integer) map.get("qty"));
+            repository.save(entity);
+        }
+
+    }
+}
